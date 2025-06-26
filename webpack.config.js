eeb7dcdf76ddd1e52c60263df3094e9301cef865
raidenkhan <iamegyimah@gmail.com>
2025-06-26 04:22:23 +0000

    working configurations

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-  // Customize the config before returning it.
+
+  config.resolve.fallback = {
+    ...(config.resolve.fallback || {}),
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'), // ← add this line
+  };
+
   return config;
 };
